8dd26150922b54214cc4f211dfa825b47df19a7d
muwah9 <muwah9@gmail.com>
2021-06-15 16:01:26 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TITLES = ['Уютная квартира', 'Дом в викторианском стиле','Тропическое бунгало', 'Двухэтажное бунгало на сваях', 'Дворец в стиле барокко'];
+const DESCRIPTION = ['Недалеко от метро', 'Окна во двор', 'Квартира с евро ремонтом', 'Необходимая инфраструкрута в доступности'];
+const TIME_CHECKS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/duonguyen-8LrGtIxxa4w.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.randomIdxpg'];
+const AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+
+const SIMILAR_COUNT = 4;
+
 // взято с https://learn.javascript.ru https://habr.com/ru/post/312880/#s2_11  https://javascript.ru/Math.round //
 
 const getRandomNumber = function (min, max) {
@@ -18,5 +28,38 @@ const getRandomCoordinate = function (min, max, afterDot) {
   return 'некоректное значение';
 };
 
-getRandomCoordinate(10, 200,6);
-getRandomNumber(10,500);
+const getRandomArrayElement = (items) => items[getRandomNumber(0, items.length - 1)];
+
+const createAdvertisement = () => {
+  const COORDINATES = {
+    lat: getRandomCoordinate(35.65, 35.7, 5),
+    lng: getRandomCoordinate(139.7, 139.8, 5),
+  };
+
+  return {
+    author: {
+      avatar: getRandomArrayElement(AVATAR),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${COORDINATES.lat}, ${COORDINATES.lng}`,
+      price: getRandomNumber(0, 10000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(1, 10),
+      guests: getRandomNumber(1, 10),
+      checkin: getRandomArrayElement(TIME_CHECKS),
+      checkout: getRandomArrayElement(TIME_CHECKS),
+      features: getRandomArrayElement(FEATURES),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: getRandomArrayElement(PHOTOS),
+      location: {
+        lat: COORDINATES.lat,
+        lng: COORDINATES.lng,
+      },
+    },
+  };
+};
+
+const similarAdvertisement = new Array(SIMILAR_COUNT).fill(null).map(() => createAdvertisement());
+
+similarAdvertisement.slice();
