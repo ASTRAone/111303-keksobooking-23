1ae716ed35224cac2a680fe0a502fdc0c85bd777
muwah9 <muwah9@gmail.com>
2021-06-16 16:53:27 +0300

    fix after review

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const TITLES = ['Уютная квартира', 'Дом в викторианском стиле','Тропическое бунгало', 'Двухэтажное бунгало на сваях', 'Дворец в стиле барокко'];
-const DESCRIPTION = ['Недалеко от метро', 'Окна во двор', 'Квартира с евро ремонтом', 'Необходимая инфраструкрута в доступности'];
-const TIME_CHECKS = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/duonguyen-8LrGtIxxa4w.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.randomIdxpg'];
-const AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const TITLES = [
+  'Уютная квартира',
+  'Дом в викторианском стиле',
+  'Тропическое бунгало',
+  'Двухэтажное бунгало на сваях',
+  'Дворец в стиле барокко',
+];
+const DESCRIPTION = [
+  'Недалеко от метро',
+  'Окна во двор',
+  'Квартира с евро ремонтом',
+  'Необходимая инфраструкрута в доступности',
+];
+const TIME_CHECKS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
 const SIMILAR_COUNT = 4;
 
@@ -28,38 +59,49 @@ const getRandomCoordinate = function (min, max, afterDot) {
   return 'некоректное значение';
 };
 
-const getRandomArrayElement = (items) => items[getRandomNumber(0, items.length - 1)];
+const shuffle = function(array) {
+  const cloneArray = array.slice(0);
+  cloneArray.sort(() => Math.random() - 0.5);
+
+  return cloneArray;
+};
+
+const getRandomArrayElement = (items) =>
+  items[getRandomNumber(0, items.length - 1)];
+
 
 const createAdvertisement = () => {
-  const COORDINATES = {
-    lat: getRandomCoordinate(35.65, 35.7, 5),
-    lng: getRandomCoordinate(139.7, 139.8, 5),
-  };
+  const lat = getRandomCoordinate(35.65, 35.7, 5);
+  const lng = getRandomCoordinate(139.7, 139.8, 5);
+  const featuresRandomIndex = getRandomNumber(1, FEATURES.length - 1);
+  const features = shuffle(FEATURES).slice(0, featuresRandomIndex);
 
   return {
     author: {
-      avatar: getRandomArrayElement(AVATAR),
+      avatar: `img/avatars/user0${getRandomNumber(1, 10)}.png`,
     },
     offer: {
       title: getRandomArrayElement(TITLES),
-      address: `${COORDINATES.lat}, ${COORDINATES.lng}`,
+      address: `${lat}, ${lng}`,
       price: getRandomNumber(0, 10000),
       type: getRandomArrayElement(TYPES),
       rooms: getRandomNumber(1, 10),
       guests: getRandomNumber(1, 10),
       checkin: getRandomArrayElement(TIME_CHECKS),
       checkout: getRandomArrayElement(TIME_CHECKS),
-      features: getRandomArrayElement(FEATURES),
+      features,
       description: getRandomArrayElement(DESCRIPTION),
       photos: getRandomArrayElement(PHOTOS),
       location: {
-        lat: COORDINATES.lat,
-        lng: COORDINATES.lng,
+        lat,
+        lng,
       },
     },
   };
 };
 
-const similarAdvertisement = new Array(SIMILAR_COUNT).fill(null).map(() => createAdvertisement());
+const similarAdvertisement = new Array(SIMILAR_COUNT)
+  .fill(null)
+  .map(() => createAdvertisement());
 
 similarAdvertisement.slice();
