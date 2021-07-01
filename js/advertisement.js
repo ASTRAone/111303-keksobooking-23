742fbfb66b401a960fd72cf449b407c77e644d57
muwah9 <muwah9@gmail.com>
2021-07-01 13:01:04 +0300

    Отрисуй меня полностью (часть 2)Ñ

--- js/advertisement.js
+++ js/advertisement.js
@@ -12,6 +12,36 @@ const checkChild = (element) => {
     element.remove();
   }
 };
+//выводим фотографии
+
+const getPhotos = function(element, offer){
+  const photosBlock = element.querySelector('.popup__photos');
+  const photoElement = photosBlock.querySelector('.popup__photo');
+  for (let index = 0; index < offer.length; index++) {
+    photoElement.src = offer;
+    checkChild(photosBlock);
+  }
+};
+//выводим все удобства
+const getFeatures = function(element, offer) {
+  const featuresListContainer = element.querySelector('.popup__features');
+  const modifiers = offer.map((feature) => `popup__feature--${feature}`);
+  featuresListContainer.querySelectorAll('.popup__feature').forEach((classItem) => {
+    const modifier = classItem.classList[1];
+    if (!modifiers.includes(modifier)) {
+      classItem.remove();
+    }
+    checkChild(featuresListContainer);
+  });
+};
+
+//выводим описание
+const getDescription = function (element, offer) {
+  element.querySelector('.popup__description').textContent = offer.description;
+  if (element.querySelector('.popup__description').textContent === undefined || element.querySelector('.popup__description').textContent === '') {
+    element.querySelector('.popup__description').remove();
+  }
+};
 
 const similarAdvertisementTemplate = document.querySelector('#card').content.querySelector('.popup');
 
@@ -32,38 +62,12 @@ createAdvertisementElement.forEach(({author, offer}) => {
   advertisementElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
-  //выводим все удобства
 
-  const featuresListContainer = advertisementElement.querySelector('.popup__features');
-  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-  featuresListContainer.querySelectorAll('.popup__feature').forEach((classItem) => {
-    const modifier = classItem.classList[1];
-    if (!modifiers.includes(modifier)) {
-      classItem.remove();
-    }
-  });
-  checkChild(featuresListContainer);
-
-  //выводим описание
-
-  advertisementElement.querySelector('.popup__description').textContent = offer.description;
-  if (advertisementElement.querySelector('.popup__description').textContent === undefined || advertisementElement.querySelector('.popup__description').textContent === '') {
-    advertisementElement.querySelector('.popup__description').remove();
-  }
-
-  //выводим фотографии
-  const getPhotos = function () {
-    const photosBlock = advertisementElement.querySelector('.popup__photos');
-    const photoElement = photosBlock.querySelector('.popup__photo');
-    for (let index = 0; index < offer.photos.length; index++) {
-      photoElement.src = offer.photos;
-      checkChild(photosBlock);
-    }
-  };
-  getPhotos();
+  getFeatures(advertisementElement,offer.features);
+  getPhotos(advertisementElement,offer.photos);
+  getDescription(advertisementElement,offer.description);
 
   advertisementElement.querySelector('.popup__avatar').src = author.avatar;
-
   similarAdvertisementFragment.appendChild(advertisementElement);
 });
 
